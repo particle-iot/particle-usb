2446148d9b1959e07effa9dcdc5ca0c89a423edd
Andrey Tolstoy <avtolstoy@gmail.com>
2019-02-06 01:03:33 +0700

    webpack: build for everything

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'particle-usb.bundle.js',
     library: 'ParticleUsb',
-    libraryTarget: 'var'
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   }
 };
