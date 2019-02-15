f0c23929898bd96f1b5c07cb18a87fe45c664f15
Sergey Polyakov <sergeuz@gmail.com>
2019-02-15 18:13:24 +0200

    Do not wait for the device to leave the listening mode; minor fixes

--- test/cellular-device.js
+++ test/cellular-device.js
@@ -1,27 +1,30 @@
 import { getDevices } from '../src/particle-usb';
 
-import { expect } from './support';
+import { expect, optionalTest } from './support';
 
-// Note: This test requires a physical device to be connected to the host via USB and is skipped by default
-describe.skip('cellular-device', function() {
+describe('cellular-device', function() {
   // Cellular device operations may take a while
   this.timeout(60000);
   this.slow(45000);
 
   let dev = null;
 
-  before(async () => {
-    let devs = await getDevices();
-    devs = devs.filter(dev => dev.isCellularDevice);
-    if (devs.length != 1) {
-      throw new Error('Make sure exactly one cellular device is connected to the host via USB');
-    }
-    dev = devs[0];
-    await dev.open();
+  before(() => {
+    return optionalTest(this, async () => {
+      let devs = await getDevices();
+      devs = devs.filter(dev => dev.isCellularDevice);
+      if (devs.length < 1) {
+        throw new Error('This test requires a cellular device connected to the host via USB');
+      }
+      dev = devs[0];
+      await dev.open();
+    });
   });
 
   after(async () => {
-    await dev.close();
+    if (dev) {
+      await dev.close();
+    }
   });
 
   describe('CellularDevice', () => {
