d33ae8d6e5a9a690551639419e20066ba00ac26a
busticated <itsbusticated@gmail.com>
2021-11-11 11:35:36 -0800

    don't mutate @particle/device-constants data, simplify usb / dfu id parsing

--- src/platforms.js
+++ src/platforms.js
@@ -1,14 +1,7 @@
 import deviceConstants from '@particle/device-constants';
 
-export const PLATFORMS = Object.values(deviceConstants); // TODO: (Julien) .filter((platform) => platform.features.include('usb-requests'));
+export const PLATFORMS = Object.values(clone(deviceConstants)); // TODO: (Julien) .filter((platform) => platform.features.include('usb-requests'));
 
-// Convert the "0x2b04" id strings to 0x2b04 numbers
-const parseIds = ({ vendorId, productId }) => {
-	return {
-		vendorId: parseInt(vendorId.replace(/0x/, ''), 16),
-		productId: parseInt(productId.replace(/0x/, ''), 16)
-	};
-};
 PLATFORMS.forEach((platform) => {
 	if (platform.usb) {
 		platform.usb = parseIds(platform.usb);
@@ -17,3 +10,16 @@ PLATFORMS.forEach((platform) => {
 		platform.dfu = parseIds(platform.dfu);
 	}
 });
+
+// Convert the "0x2b04" id strings to 0x2b04 numbers
+function parseIds({ vendorId, productId }){
+	return {
+		vendorId: Number(vendorId),
+		productId: Number(productId)
+	};
+}
+
+function clone(x){
+	return JSON.parse(JSON.stringify(x));
+}
+
