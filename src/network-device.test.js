ce9c6d8bf81ca72db2d32a03463bcf89f77347b0
Joe Goggins <joe@particle.io>
2022-02-03 15:27:50 -0600

    Add assertions for NetworkDevice (deprecated methods)

--- src/network-device.test.js
+++ src/network-device.test.js
@@ -1,5 +1,12 @@
-describe('NetworkDevice', () => {
-	it('contains all deprecated methods	(getNetworkStatus, getNetworkConfig, setNetworkConfig) that are not worth testing', async () => {
-
-	});
+const { expect } = require('../test/support');
+const { setDevicePrototype } = require('./set-device-prototype');
+describe('NetworkDevice (all deprecated methods)', () => {
+	const deprecatedMethods = ['getNetworkStatus', 'getNetworkConfig', 'setNetworkConfig'];
+	for (const method of deprecatedMethods) {
+		it(`provides ${method}`, async () => {
+			const fakeDevice = { type: 'p2' }; // could be any device type
+			const result = setDevicePrototype(fakeDevice);
+			expect(result[method]).to.be.a('Function');
+		});
+	}
 });
