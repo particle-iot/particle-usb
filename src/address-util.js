efab7e4972f058c7a24e42cd93e7defb84b2c6bd
Julien Vanier <julien@particle.io>
2024-01-26 09:48:09 -0500

    Handle case where MAC address is missing

--- src/address-util.js
+++ src/address-util.js
@@ -1,4 +1,8 @@
 function convertBufferToMacAddress(buffer) {
+	if (!buffer) {
+		return buffer;
+	}
+
 	const bytes = Array.from(buffer);
 	return bytes.map(byte => byte.toString(16).padStart(2, '0')).join(':');
 }
