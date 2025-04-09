070dbb78a067744efc75cc66660c2716c77582c1
Julien Vanier <julien@particle.io>
2025-04-09 12:03:27 -0400

    Calculate Tachyon device ID in EDL mode with the same algorithm as API

--- src/edl-device.js
+++ src/edl-device.js
@@ -1,4 +1,5 @@
 const { getUsbDevices } = require('./usb-device-node');
+const deviceConstants = require('@particle/device-constants');
 
 const VENDOR_ID_QUALCOMM = 0x05c6;
 const PRODUCT_ID_EDL_DEVICE = 0x9008;
@@ -6,6 +7,16 @@ const PRODUCT_ID_EDL_DEVICE = 0x9008;
 class EdlDevice {
 	constructor({ serialNumber }) {
 		this.serialNumber = serialNumber;
+		this.id = this._computeDeviceId();
+	}
+
+	_computeDeviceId() {
+		// Assume all EDL devices are Tachyons and use the SOC machine ID type
+		const platformId = deviceConstants.tachyon.id;
+		const machineIdType = deviceConstants.linux.machineIdTypes.SOC;
+		const platformAndMachineIdType = Buffer.from([platformId, machineIdType]).toString('hex');
+		const suffix = this.serialNumber.toLowerCase().substring(0, 18).padStart(18, '0');
+		return `42${platformAndMachineIdType}${suffix}`;
 	}
 
 	static async getEdlDevices() {
